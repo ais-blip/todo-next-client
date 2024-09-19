eead8b302ec45d0c0b58e353782b12dba223a8e4
ais-blip <gusadeai@student.gu.se>
2024-09-19 10:14:05 +0200

    Update useTodos.js

--- components/useTodos.js
+++ components/useTodos.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 
-const SERVER_ENDPOINT = 'https://humble-space-giggle-4qw9qjp9rph7vwq-5000.app.github.dev'
+const SERVER_ENDPOINT = 'https://potential-invention-694vv7q7g6p5hr74-5000.app.github.dev/'
 
 export const useTodos = () => {
   const [todos, setTodos] = useState([]);
